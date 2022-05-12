5ab0f97ef4139f101d8e57d3d656e5dbe8fa8a67
user <xoja9636706@gmail.com>
2022-05-12 13:23:08 +0500

    as

--- src/components/News/index.jsx
+++ src/components/News/index.jsx
@@ -1,31 +1,33 @@
-import React from 'react';
+import React from "react";
 
-import LatestNews from '../LatestNews/';
-import TopNews from '../TopNews/';
-import NewsFilter from '../NewsFilter/'
-import UrgentNews from '../UrgentNews/'
-import OtherNews from '../OtherNews/'
+import LatestNews from "../LatestNews/";
+import TopNews from "../TopNews/";
+import NewsFilter from "../NewsFilter/";
+import UrgentNews from "../UrgentNews/";
+import OtherNews from "../OtherNews/";
 
 import "./news.scss";
 
 const News = () => {
-  return (
-    <div className="news">
-      <div className="news__container">
-        <div className="top__news">
-          <TopNews />
+    return (
+        <div className="news">
+            <div className="news__container">
+                <div className="top__news">
+                    <TopNews />
+                </div>
+                <div className="latest__news">
+                    <LatestNews />
+                </div>
+            </div>
+            <NewsFilter />
+            <div className="other__news">
+                <div className="mini__container">
+                    <UrgentNews />
+                    <OtherNews />
+                </div>
+            </div>
         </div>
-        <div className="latest__news">
-          <LatestNews />
-        </div>
-      </div>
-      <NewsFilter />
-      <div className="other__news">
-        <UrgentNews />
-        <OtherNews />
-      </div>
-    </div>
-  )
-}
+    );
+};
 
 export default News;
--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,35 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-import Zoom from '../../assets/images/search/zoom.svg'
-import Weather from '../../assets/images/search/weather.svg'
-import Flag from '../../assets/images/search/flag.svg'
-import RightArrow from '../../assets/images/search/right_arrow.svg'
-
+import Zoom from "../../assets/images/search/zoom.svg";
+import Weather from "../../assets/images/search/weather.svg";
+import Flag from "../../assets/images/search/flag.svg";
+import RightArrow from "../../assets/images/search/right_arrow.svg";
+import axios from "axios";
+import { BiMoon } from 'react-icons/bi';
+import { BiSun } from 'react-icons/bi';
 import "./search.scss";
 
 const Search = () => {
-  return (
-    <div className="search__wrapper">
-      <div className="search__container">
-        <div className="search__container__items">
-          <div className="search__container__items__input">
-            <input type="text" placeholder="ПОИСК..." />
-            <img src={ Zoom } alt="" />
-          </div>
-          <div className="search__container__items__weather">
-            <img src={ Weather } alt="" className="search__container__items__weather__logo" />
-            <span>Сейчас +15</span>
-            <img src={ RightArrow } alt="" />
-          </div>
-          <div className="search__container__items__currency">
-            <img src={ Flag } alt="" className="search__container__items__currency__logo" />
-            <span>USD 11 260,2 </span>
-            <img src={ RightArrow } alt="" />
-          </div>
+    const [homedetails, setHomedetails] = useState("");
+    useEffect(() => {
+        axios
+            .get("https://businessnews.uz/backend/v1/api/main/home-details/")
+            .then((res) => {
+                setHomedetails(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
+    
+    function cons() {
+        console.log("homedetails", homedetails);
+    }
+
+    return (
+        <div className="search__wrapper">
+            <div className="search__container">
+                <div className="search__container__items">
+                    <div className="search__container__items__input">
+                        <input type="text" placeholder="ПОИСК..." />
+                        <img src={Zoom} alt="" />
+                    </div>
+                    <div className="search__container__items__weather">
+                        { homedetails.icon !== "01d"  & homedetails.icon !== "01n" ? <img
+                            src={`http://openweathermap.org/img/wn/${homedetails.icon}.png`}
+                            alt=""
+                            className="search__container__items__weather__logo"
+                        /> : ""}
+                        { homedetails.icon === "01d" ? <BiSun style={{color:"orange" , fontSize:"28px"}} /> : ""}
+                        { homedetails.icon === "01n" ? <BiMoon style={{ fontSize:"28px"}} /> : ""}
+                        <span>
+                            Сейчас{" "}
+                            {homedetails.current_temp < 0
+                                ? homedetails.current_temp.slice(0, 3)
+                                : `+${homedetails.current_temp}`.slice(0, 3)}
+                        </span>
+                        <img src={RightArrow} alt="" />
+                    </div>
+                    <div className="search__container__items__currency">
+                        <img
+                            src={Flag}
+                            alt=""
+                            className="search__container__items__currency__logo"
+                        />
+                        <span>USD {homedetails.USD} </span>
+                        <img src={RightArrow} alt="" onClick={() => cons()} />
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
-}
+    );
+};
 
 export default Search;
